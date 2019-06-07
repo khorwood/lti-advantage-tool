eed018587a0f07021b69f097582254753843ffee
Kristian Horwood <khorwood@gmail.com>
2019-06-07 15:11:24 -0400

    Implement OpenID Connect flow (#3)

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # lti-advantage-tool
+
 A test harness which implements an IMS LTI Advantage tool
+
+## Initial setup
+
+Generate a TLS key-pair by running the following command. You will need to add
+the public key to your platform's trusted key store.
+
+```sh
+openssl req -nodes -new -x509 -keyout server.key -out server.cert
+```
+
+### config.json
+
+Create a config.json file with the following details.
+
+```json
+{
+    "platform_configs": [
+        {
+            "client_id": "<client id>",
+            "audience": "<auth service required audience>",
+            "issuer": "<platform issuer value>",
+            "authenticate_uri": "<platform authenticate uri>",
+            "public_key_uri": "<platform public key uri>",
+            "token_uri": "<auth service token uri>"
+        }
+    ]
+}
+```
--- src/jose.js
+++ src/jose.js
@@ -1,12 +1,17 @@
 'use strict';
 
-const debug = require('debug')('lti-advantage-tool:jwk');
+const debug = require('debug')('lti-advantage-tool:jose');
 const jose = require('node-jose');
+const request = require('request-promise-native');
 
 class JOSE {
-    constructor() {
+    constructor(platformConfigs) {
+        this._platformConfigs = platformConfigs;
+
         this._keystore = jose.JWK.createKeyStore();
         this._keystore.generate('RSA', 2048, { alg: 'RS256', use: 'sig' });
+
+        this._publicKeys = jose.JWK.createKeyStore();
     }
 
     /**
@@ -14,8 +19,8 @@ class JOSE {
      * @param {} req The request
      * @param {*} res The response
      */
-    async getJwks(req, res) {
-        debug('getJwks');
+    async get_jwks(req, res) {
+        debug('get_jwks');
 
         const response = this._keystore.toJSON();
 
@@ -27,13 +32,55 @@ class JOSE {
      * @param {string} payload The payload as a string
      */
     async sign(payload) {
+        debug('sign');
+
         const key = this._keystore.all()[0];
 
-        const opts = { compact: true, jwk: key, fields: { typ: 'jwt' } };
+        const opts = { compact: true, jwk: key };
 
         return await jose.JWS.createSign(opts, key)
             .update(payload)
             .final();
+    }
+
+    /**
+     * Validates a signed JWT against the keystore
+     * @param {string} token The JWT to validate
+     */
+    async validate(token) {
+        debug('validate');
+
+        await this.fetch_platform_keys();
+
+        let result = await jose.JWS.createVerify(this._publicKeys)
+            .verify(token);
+
+        return result;
+    }
+
+    async fetch_platform_keys() {
+        debug('fetch_platform_keys');
+
+        for (let config of this._platformConfigs) {
+            debug('fetching public keys', config.public_key_uri);
+            try {
+                let jwks = await request.get({
+                    json: true,
+                    timeout: 500,
+                    uri: config.public_key_uri
+                });
+                for (let jwk of jwks.keys) {
+                    let key = this._publicKeys.get(jwk.kid)
+                    if (!key) {
+                        debug('adding key', jwk.kid);
+                        await this._publicKeys.add(jwk, 'json');
+                    }
+                }
+            } catch (e) {
+                debug('unable to fetch JWKS', config.public_key_uri);
+                debug(e.message);
+            }
+        }
     }
 }
 
