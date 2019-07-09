ac83ed1da524043cd28b3c739844b2eb1958cb80
Kristian Horwood <khorwood@gmail.com>
2019-07-09 11:14:11 -0400

    Update readme (#10)

--- src/routes/links.js
+++ src/routes/links.js
@@ -10,5 +10,6 @@ router.get('/lineitem', controller.lineitem_link);
 router.post('/lineitem_form', controller.lineitem_form);
 router.get('/lineitemscore', controller.lineitemscore_link);
 router.post('/lineitemscore_form', controller.lineitemscore_form);
+router.get('/nrpslink', controller.nrps_link);
 
 module.exports = router;
